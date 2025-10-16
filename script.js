46f0c5ae39f07cb0f41c0d01e15481b136d9359d
souza-ops <matheus.de.souza18@escola.pr.gov.br>
2025-10-16 10:39:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const avanca = document.queryselectorall('.btn-proximo');
 
+avanca.forEach(button => {
+button.addEventLister('click', function(){
+  const atual = document.querySelector('.ativo)
+ const proximoPasso = 'passo-' + this.getAttibute('data-proximo');                                 
+
+atual.classList.remove('ativo');
+  document.getElementById(proximoPasso).classList.add('ativo');
+})
+})
